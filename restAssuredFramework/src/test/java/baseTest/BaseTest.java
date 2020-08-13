2a8c05ddfb8022c3618176bf7602eec8f3117529
sjunaid <sjunaid@PAS-SJUNAID-L3.nextestate.com>
2020-08-13 07:34:25 -0700

    Add Extentreport listener for logging

--- restAssuredFramework/src/test/java/baseTest/BaseTest.java
+++ restAssuredFramework/src/test/java/baseTest/BaseTest.java
@@ -1,14 +1,20 @@
 package baseTest;
 
+
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.LogStatus;
+
+import utils.ExtentReportListener;
 import utils.FileandEnv;
 
-public class BaseTest {
+public class BaseTest extends ExtentReportListener {
 	
 	@Test
 	public void utilsTest()
 	{
-		System.out.println(FileandEnv.envAndFile());
+		test.log(LogStatus.INFO, "Starting Test");
+		//test.log(LogStatus.PASS,"test Pass");
+		System.out.println(FileandEnv.envAndFile().get("ServerUrl"));
 	}
 }
